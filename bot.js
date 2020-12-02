5ab7d85a1521e0bcfd5604689ecdb17d56903218
Casper Haems <casper.haems@ugent.be>
2020-12-02 16:11:45 +0100

    initial commit

---
+++ bot.js
@@ -0,0 +1,34 @@
+// Run dotenv
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase().includes("bier")) {
+    msg.react('🍻');
+  }
+  if (msg.content.toLowerCase().includes("ideefix") && !msg.content.includes(":ideefix:")) {
+	  const ayy = client.emojis.cache.find(emoji => emoji.name === "ideefix");
+	  msg.react(ayy);
+  }
+  if (msg.content.toLowerCase().includes("hospitalia") && !msg.content.includes(":hospitalia:")) {
+	  const ayy = client.emojis.cache.find(emoji => emoji.name === "hospitalia");
+	  msg.react(ayy);
+  }
+  if (msg.content.toLowerCase().includes("acantha") && !msg.content.includes(":acantha:")) {
+	  const ayy = client.emojis.cache.find(emoji => emoji.name === "acantha");
+	  msg.react(ayy);
+  }
+  if (msg.content.toLowerCase().includes("regent") && !msg.content.includes(":regent:")) {
+	  const ayy = client.emojis.cache.find(emoji => emoji.name === "regent");
+	  msg.react(ayy);
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
