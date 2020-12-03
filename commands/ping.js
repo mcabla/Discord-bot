4dad4fd2ac10de216f3ef24fb53897329db1b07d
Casper Haems <casper.haems@ugent.be>
2020-12-03 14:55:53 +0100

    fix: ping should not require arguments

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'ping',
     description: 'Ping!',
     cooldown: 5,
-    args: true,
+    args: false,
     usage: false, // Can be a string with an explanation of the required arguments
     guildOnly: false,
     execute(message, args) {
