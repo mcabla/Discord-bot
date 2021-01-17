ce3d706b335e40794088eccb9ad22ac65ea6e398
Casper Haems <casper.haems@ugent.be>
2021-01-17 21:14:52 +0100

    add: Express to allow running the bot on Replit using Uptimerobot

--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
 import {CustomClient} from "./src/Client/CustomClient";
 import {DISCORD_TOKEN} from "./src/Config/Config";
 
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req: any, res: any) => res.send('Hello World!'));
+app.listen(3000, () => console.log(`Example app listening at http://localhost:${port}`));
+
 // const fetch = require('node-fetch');
 const client = new CustomClient();
 
