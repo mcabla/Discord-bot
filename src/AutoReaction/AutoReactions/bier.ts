68edc5c5a30a3110b9ac7c1b8064aea811774da2
Casper Haems <casper.haems@ugent.be>
2021-02-01 12:29:51 +0100

    add: pint as synonym for beer

--- src/AutoReaction/AutoReactions/bier.ts
+++ src/AutoReaction/AutoReactions/bier.ts
@@ -3,7 +3,7 @@ import {AAutoReaction} from "../AAutoReaction";
 
 export default class Bier extends AAutoReaction {
     name = 'bier';
-    aliases = ['beer', 'gerstenat'];
+    aliases = ['beer', 'gerstenat', 'pint'];
     description = 'reacts with beer emoji';
     execute(message: Message) {
         message?.react('🍻').then();
