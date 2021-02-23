4c348a85e448d9063dd4da225d6ec7163a18d06b
Casper Haems <casper.haems@ugent.be>
2021-02-23 15:31:40 +0100

    add: bot command that lists all bots

---
+++ src/Handler/Command/Commands/bot.ts
@@ -0,0 +1,21 @@
+import {DMChannel, Message} from "discord.js";
+import {ACommand} from "../ACommand";
+
+export default class Bot extends ACommand {
+    name = 'bot';
+    aliases = ['bots'];
+    description = 'Print al bots';
+    usage = '';
+    guildOnly = true;
+    execute(message: Message, args: string[]) {
+        if (message.channel instanceof DMChannel){
+            return;
+        }
+
+       const bots = message.channel.members
+           .filter(member => member.user.bot)
+           .map((v,k) => `🔹 <@${v.id}>`)
+           .join('\n');
+       message.reply(`The channel has the following bots:\n${bots}`).then();
+    }
+}
